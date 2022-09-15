fb8390817999aea4e26ba898e2d1f5919c9bc106
Tom <1547107965@qq.com>
2022-09-15 10:53:35 +0800

    coding sql

---
+++ src/main/java/oh/awesome/flink/table/UnifyDynamicTableSource.java
@@ -0,0 +1,28 @@
+package oh.awesome.flink.table;
+
+import org.apache.flink.table.connector.ChangelogMode;
+import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.connector.source.ScanTableSource;
+
+public class UnifyDynamicTableSource implements ScanTableSource {
+
+    @Override
+    public ChangelogMode getChangelogMode() {
+        return ChangelogMode.all();
+    }
+
+    @Override
+    public ScanRuntimeProvider getScanRuntimeProvider(ScanContext runtimeProviderContext) {
+        return null;
+    }
+
+    @Override
+    public DynamicTableSource copy() {
+        return null;
+    }
+
+    @Override
+    public String asSummaryString() {
+        return String.join("-", UnifySourceDynamicTableFactory.IDENTIFIER, "source");
+    }
+}
--- src/main/java/oh/awesome/flink/table/UnifySourceDynamicTableFactory.java
+++ src/main/java/oh/awesome/flink/table/UnifySourceDynamicTableFactory.java
@@ -4,7 +4,12 @@ import org.apache.flink.configuration.ConfigOption;
 import org.apache.flink.table.connector.source.DynamicTableSource;
 import org.apache.flink.table.factories.DynamicTableSourceFactory;
 
+import oh.awesome.flink.config.KafkaMessageSourceOptions;
+import oh.awesome.flink.config.MysqlSnapshotSourceOptions;
+
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class UnifySourceDynamicTableFactory implements DynamicTableSourceFactory {
     public static final String IDENTIFIER = "hybrid";
@@ -21,11 +26,29 @@ public class UnifySourceDynamicTableFactory implements DynamicTableSourceFactory
 
     @Override
     public Set<ConfigOption<?>> requiredOptions() {
-        return null;
+        return Stream.of(
+                MysqlSnapshotSourceOptions.HOST,
+                MysqlSnapshotSourceOptions.PORT,
+                MysqlSnapshotSourceOptions.USERNAME,
+                MysqlSnapshotSourceOptions.PASSWORD,
+                MysqlSnapshotSourceOptions.SCHEMA_NAME,
+                MysqlSnapshotSourceOptions.TABLE_NAME,
+                MysqlSnapshotSourceOptions.SPLIT_COLUMN,
+                KafkaMessageSourceOptions.TOPIC,
+                KafkaMessageSourceOptions.TOPIC_PATTERN,
+                KafkaMessageSourceOptions.PROPS_BOOTSTRAP_SERVERS,
+                KafkaMessageSourceOptions.PROPS_GROUP_ID,
+                KafkaMessageSourceOptions.VALUE_FORMAT
+        ).collect(Collectors.toSet());
     }
 
     @Override
     public Set<ConfigOption<?>> optionalOptions() {
-        return null;
+        return Stream.of(
+                MysqlSnapshotSourceOptions.SPLIT_NUM,
+                MysqlSnapshotSourceOptions.SOURCE_READER_QUEUE_CAPACITY,
+                MysqlSnapshotSourceOptions.SOURCE_READER_SPLIT_FETCHER_NUM,
+                MysqlSnapshotSourceOptions.SOURCE_READER_PROJECT_COLUMNS
+        ).collect(Collectors.toSet());
     }
 }
